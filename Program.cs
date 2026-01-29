379d2daa899f8dc914eb061e927f6bbc06302d48
Mickael Randr <micrandr@gmail.com>
2026-01-29 23:51:39 +0300

    chores: use pages, routing, and layouts with blazor

--- Program.cs
+++ Program.cs
@@ -1,14 +1,16 @@
 using BlazingPizza.Data;
+using BlazingPizza.Services;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-
 builder.Services.AddHttpClient();
 builder.Services.AddSqlite<PizzaStoreContext>("Data Source=pizza.db");
+builder.Services.AddScoped<OrderState>();
 
 var app = builder.Build();
-
 
 if (!app.Environment.IsDevelopment())
 {
@@ -23,7 +25,6 @@ app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
 app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
 
-// Initialize the database
 var scopeFactory = app.Services.GetRequiredService<IServiceScopeFactory>();
 using (var scope = scopeFactory.CreateScope())
 {
